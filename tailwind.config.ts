7febd7501060149f732b7529f494815e577e1acd
miguel prada <im.mprada@gmail.com>
2024-03-20 14:50:58 -0500

    add styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,32 @@
 import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
-      }
+        sans: 'Poppins, sans-serif',
+      },
+      colors: {
+        'cyan': '#2BD0D0',
+        'light-grey': '#EFF1F7',
+        grey: '#9E9AA8',
+        'dark-grey': '#34313D',
+        'purple-grey': '#3A3054',
+        black: '#34313D',
+      },
+      fontSize: {
+        sm: '0.9375rem', //15px
+        base: '1rem', //16px
+        lg: '1.125rem', //18px
+        xl: '1.25rem', //20px
+        '2xl': '1.375rem',
+        '5xl': '2.5rem',
+        '6xl': '5rem', //80px
+      },
     },
   },
   plugins: [],
