58e8f8d9c1156677d2af8460585c31d6de046cb1
immprada <im.mprada@gmail.com>
2024-04-17 12:02:59 -0500

    implement UrlsContext

--- src/components/url-form/index.tsx
+++ src/components/url-form/index.tsx
@@ -1,20 +1,37 @@
+import { useContext, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import { UrlsContext } from "../../contexts/urls-context";
 
 export default function UrlForm() {
+  const [urlToSubmit, setUrlToSubmit] = useState<string>('');
+  const { submiting, handleSubmit } = useContext(UrlsContext);
+  
+
   return (
     <div className="flex mt-6 xl:mt-10 flex-col relative">
       <div className="bg-white w-full h-20"></div>
       <div className="bg-light-grey w-full h-20"></div>
       <div className="w-full max-xl:px-4 absolute top-0">
-        <form className="bg-purple-bg max-w-[1110px] xl:mx-auto h-40 py-7 xl:py-14 px-8 xl:px-16 rounded-xl flex max-xl:flex-col max-xl:items-center gap-4 max-xl:gap-2">
+        <form
+          className="bg-purple-bg max-w-[1110px] xl:mx-auto h-40 py-7 xl:py-14 px-8 xl:px-16 rounded-xl flex max-xl:flex-col max-xl:items-center gap-4 max-xl:gap-2"
+          onSubmit={(e) => {handleSubmit(e, urlToSubmit)}}
+        >
           <input
             type="text"
             className="py-3.5 px-8 font-sans text-grey rounded-xl w-full flex-1"
+            name="url"
+            onChange={(e) => {setUrlToSubmit(e.target.value)}}
           />
           <button
             type="submit"
             className="bg-cyan text-white py-3.5 px-8 rounded-xl font-sans font-bold max-xl:w-full"
+            disabled={submiting}
           >
-            Shorten it!
+            {submiting ? (
+              <ClipLoader color="white" loading={submiting} size={15} />
+            ) : (
+              'Shorten it!'
+            )}
           </button>
         </form>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,15 @@ import FeatureCards from "../components/feature-cards"
 import Footer from "../components/footer"
 import Hero from "../components/hero"
 import UrlForm from "../components/url-form"
+import { UrlsProvider } from "../contexts/urls-context"
 
 export default function Main() {
   return (
     <div>
       <Hero/>
-      <UrlForm/>
+      <UrlsProvider>
+        <UrlForm/>
+      </UrlsProvider>
       <FeatureCards/>
       <Footer/>
     </div>
